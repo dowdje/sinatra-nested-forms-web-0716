b1bae9f206c3f72ee9c13b46a538ec50c8768a07
dowdje <dowdje89@gmail.com>
2016-08-12 18:06:27 -0400

    PIrate Kings

--- app.rb
+++ app.rb
@@ -3,7 +3,30 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+      configure do
+    set :public_folder, 'public'
+    set :views, 'views/pirates'
+  end
+
+    get '/' do
+      erb :index
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
+
+      @ships = Ship.all
+
+      erb :show
+    end
 
   end
 end
